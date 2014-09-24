62d2daec3fac04179c9f24e41bfec700ef0ea737
vgamekid3785 <jeinstein63@gmail.com>
2014-09-23 22:54:47 -0400

    Added to ability to scrape searches on craigslist.

--- IntUIScrape/IntUIScrape/spiders/craigslst.py
+++ IntUIScrape/IntUIScrape/spiders/craigslst.py
@@ -8,12 +8,12 @@ from scrapy.http    import Request
 
 class Craig(CrawlSpider):
     name = "craig"
-    rules = ( Rule(SgmlLinkExtractor(allow=("index\d00\.html", ), restrict_xpaths=('//a[@class="button next"]' ,)), callback="parse_items", follow=True), )
+    rules = ( Rule(SgmlLinkExtractor(allow=("search/vga/\?s=100&","/^s/", ), restrict_xpaths=('//a[@class="button next"]' ,)), callback="parse_items", follow=True), )
 
     def __init__(self, city, category):
         super(Craig, self).__init__()
         self.city = city
-        self.start_urls = ['http://' + str(city) + '.craigslist.org/'+str(category)]
+        self.start_urls = ['http://' + str(city) + '.craigslist.org/search/'+str(category)+"?query="]
         self.allowed_domains = [str(city)+".craigslist.org"]
         
 
@@ -35,5 +35,60 @@ class Craig(CrawlSpider):
         item = response.meta['item']
         item['description'] = hxs.xpath('//section[@id="postingbody"]/text()').extract()
         return item
+
+    def parse_start_url(self, response):
+        hxs = Selector(response)
+        rows = hxs.xpath('//div[@class="content"]/p[@class="row"]')
+        for row in rows:
+            item = Craigslist()
+            link = row.xpath('.//span[@class="pl"]/a')
+            url = 'http://'+self.city+'.craigslist.org{}'.format(''.join(link.xpath("@href").extract()))
+            item['title'] = link.xpath("text()").extract()
+            item['link'] = url
+            item['price'] = row.xpath('.//span[@class="l2"]/span[@class="price"]/text()').extract()
+            yield Request(url=url, meta={'item': item}, callback=self.parse_item_page)
+
+class CraigSearch(CrawlSpider):
+    name = "craigSearch"
+
+
+    def __init__(self, city, searchString):
+        super(CraigSearch, self).__init__()
+        self.city = city
+        self.searchString = searchString
+        self.start_urls = ['http://'+city+'.craigslist.org/search/sss?query='+searchString+'&sort=rel']
+        self.allowed_domains = [str(city)+".craigslist.org"]
+        self.rules = ( Rule(SgmlLinkExtractor(allow=('search/sss?s=\d00&query='+ searchString+'&sort=rel', ), restrict_xpaths=('//a[@class="button next"]' ,)), callback="parse_items", follow=True), )
+
+    def parse_items(self, response):
+        hxs = Selector(response)
+        rows = hxs.xpath('//div[@class="content"]/p[@class="row"]')
+        for row in rows:
+            item = Craigslist()
+            link = row.xpath('.//span[@class="pl"]/a')
+            url = 'http://'+self.city+'.craigslist.org{}'.format(''.join(link.xpath("@href").extract()))
+            item['title'] = link.xpath("text()").extract()
+            item['link'] = url
+            item['price'] = row.xpath('.//span[@class="l2"]/span[@class="price"]/text()').extract()
+            yield Request(url=url, meta={'item': item}, callback=self.parse_item_page)
+
+    def parse_item_page(self, response):
+        hxs = Selector(response)
+
+        item = response.meta['item']
+        item['description'] = hxs.xpath('//section[@id="postingbody"]/text()').extract()
+        return item
+
+    def parse_start_url(self, response):
+        hxs = Selector(response)
+        rows = hxs.xpath('//div[@class="content"]/p[@class="row"]')
+        for row in rows:
+            item = Craigslist()
+            link = row.xpath('.//span[@class="pl"]/a')
+            url = 'http://'+self.city+'.craigslist.org{}'.format(''.join(link.xpath("@href").extract()))
+            item['title'] = link.xpath("text()").extract()
+            item['link'] = url
+            item['price'] = row.xpath('.//span[@class="l2"]/span[@class="price"]/text()').extract()
+            yield Request(url=url, meta={'item': item}, callback=self.parse_item_page)
 
 # 'http://'+city+'.craigslist.org/search/sss?query='+searchString+'&sort=rel'
